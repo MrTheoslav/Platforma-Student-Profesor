3ca1218f711514fc9bec67e659a08657582bb471
MrTheoslav <wo303232@student.polsl.pl>
2024-06-11 16:22:38 +0200

    - Added functionality to add/remove student by teacher/admin to/from repository - Added fuctionalities to assignments for teacher

--- Platforma_Student_Profesor/Controllers/RepositoryController.cs
+++ Platforma_Student_Profesor/Controllers/RepositoryController.cs
@@ -46,7 +46,7 @@ namespace API.Controllers
         public IActionResult GetRepositoryById(int repositoryID)
         {
 
-            if (!_repositoryService.RepositoryExist(repositoryID))
+            if (!_repositoryService.RepositoryExists(repositoryID))
                 return NotFound();
 
             var repositoryDTO = _mapper.Map<RepositoryDTO>(_repositoryService.GetRepositoryByID(repositoryID));
@@ -97,7 +97,7 @@ namespace API.Controllers
             if (!ModelState.IsValid)
                 return BadRequest("Coś poszło nie tak");
 
-            if (!_repositoryService.RepositoryExist(repositoryUpdate.RepositoryID))
+            if (!_repositoryService.RepositoryExists(repositoryUpdate.RepositoryID))
                 return BadRequest("Nie istenieje takie repozytorium");
 
             //CreateBy must be same as before!!!!
@@ -110,7 +110,114 @@ namespace API.Controllers
             return Ok("Zaktualizowano repozytorium");
         }
 
+        [Authorize(Roles ="admin,teacher")]
+        [HttpDelete]
+        public IActionResult RemoveStudentFromRepository([FromBody]UserRepositoryDTO userRepository)
+        {
+            if (userRepository == null)
+                return BadRequest("Nie można wyrzucić pustego ucznia");
+
+            if (!ModelState.IsValid)
+                return BadRequest("Coś poszło nie tak.");
+
+            if (!_repositoryService.UserExistsInRepository(userRepository.UserID, userRepository.RepositoryID))
+                return NotFound("Dane repozytorium nie posiada tego ucznia.");
+
+            var userRepositoryMap = _mapper.Map<UserRepository>(userRepository);
 
 
+            if (!_repositoryService.RemoveStudentFromRepository(userRepositoryMap))
+                return BadRequest("Coś poszło nie tak podczas usuwania ucznia z repozytorium.");
+
+            return Ok("Usunięto ucznia z repozytorium");
+        }
+
+        [Authorize(Roles ="admin,teacher")]
+        [HttpPost]
+        public IActionResult AddStudentToRepository([FromBody]UserRepositoryDTO userRepository)
+        {
+            if (userRepository == null)
+                return BadRequest("Nie można dodać pustego ucznia.");
+
+            if (!ModelState.IsValid)
+                return BadRequest("Coś poszło nie tak.");
+
+            if (_repositoryService.UserExistsInRepository(userRepository.UserID, userRepository.RepositoryID))
+                return BadRequest("Użytkownik już istnieje w tym repozytorium.");
+
+            var userRepositoryMap = _mapper.Map<UserRepository>(userRepository);
+
+            if (!_repositoryService.AddStudentToRepository(userRepositoryMap))
+                return BadRequest("Coś poszło nie tak podczas dodawania ucznia do repozytorium.");
+
+            return Ok("Dodano ucznia do repozytorium");
+        }
+
+        [Authorize(Roles = "admin,teacher")]
+        [HttpPost]
+        public IActionResult AddAssignment(AssignmentDTO assignment) 
+        {
+            if (assignment == null)
+                return BadRequest("Zadanie nie może być puste.");
+
+            if (!ModelState.IsValid)
+                return BadRequest("Coś poszło nie tak.");
+
+            if (_repositoryService.AssignmentExists(assignment.Name))
+                return BadRequest("Zadanie już istnieje.");
+
+            var assignmentMap = _mapper.Map<Assignment>(assignment);
+
+            if (!_repositoryService.AddAssignment(assignmentMap))
+                return BadRequest("Coś poszło nie tak podczas tworzenia zadania.");
+
+            return Ok("Zadanie zostało poprawnie utworzone.");
+        }
+
+        [Authorize(Roles = "admin,teacher")]
+        [HttpPut]
+        public IActionResult UpdateAssignment(AssignmentDTO assignmentUpdate)
+        {
+            if (assignmentUpdate == null)
+                return BadRequest("Zadanie do zmiany nie może być puste");
+
+            if (!ModelState.IsValid)
+                return BadRequest("Coś poszło nie tak");
+
+            if (!_repositoryService.AssignmentExists(assignmentUpdate.AssignmentID))
+                return BadRequest("Nie istenieje takie zadanie");
+
+            //CreateBy must be same as before!!!!
+            var assignmentMap = _mapper.Map<Assignment>(assignmentUpdate);
+
+
+            if (!_repositoryService.UpdateAssignment(assignmentMap))
+                return BadRequest("Coś poszło nie tak podczas zmiany");
+
+            return Ok("Zaktualizowano zadanie");
+        }
+
+        [Authorize(Roles = "admin,teacher")]
+        [HttpDelete]
+        public IActionResult DeleteAssignment(AssignmentDTO assignment)
+        {
+            if (assignment == null)
+                return BadRequest("Zadanie do usunięcia nie może być puste");
+
+            if (!ModelState.IsValid)
+                return BadRequest("Coś poszło nie tak");
+
+            if (!_repositoryService.AssignmentExists(assignment.AssignmentID))
+                return BadRequest("Nie istenieje takie zadanie");
+
+            //CreateBy must be same as before!!!!
+            var assignmentMap = _mapper.Map<Assignment>(assignment);
+
+
+            if (!_repositoryService.DeleteAssignment(assignmentMap))
+                return BadRequest("Coś poszło nie tak podczas usuwania");
+
+            return Ok("Usunięto zadanie");
+        }
     }
 }
