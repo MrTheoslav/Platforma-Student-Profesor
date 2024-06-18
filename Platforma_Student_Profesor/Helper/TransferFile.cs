27b24ae7e70238fafa0d303dc3ec118b00a0eb95
MrTheoslav <wo303232@student.polsl.pl>
2024-06-18 23:45:12 +0200

    Fixes with uploading files

--- Platforma_Student_Profesor/Helper/TransferFile.cs
+++ Platforma_Student_Profesor/Helper/TransferFile.cs
@@ -4,7 +4,10 @@ namespace API.Helper
 {
     public class TransferFile
     {
-        public FileDTO file { get; set; }
+        //public FileDTO file { get; set; }
+        public int UserID { get; set; }
+        public int AssigmentID { get; set; }
+
         public List<IFormFile> files { get; set; }
     }
 }
